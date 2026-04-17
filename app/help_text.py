6db491bdb0cb20f406d0d4a1557847f08021bf37
Garno2506 <mickgarno2506@gmail.com>
2026-04-17 09:19:27 +0100

    add help text to help_text.py

--- app/help_text.py
+++ app/help_text.py
@@ -6,15 +6,23 @@ HELP_COL1 = """
         <li>Default password: <b>letmein</b></li>
         <li>Change your password after first login</li>
         <li>Never share your password with others if you want to keep it secure</li>
+        <li>Now navigate to the library button where you will find a host of media options</li>
     </ul>
 
-    <h2 style="color: white;">Getting Started</h2>
+
+
+    <h2 style="color: white;">Adding Media To Your Library</h2>
 
     <ul>
-        <li>Default password: <b>letmein</b></li>
-        <li>Change your password after first login</li>
-        <li>Never default your password with others Garno</li>
-    </ul>
+    <li>You must be logged in to add media to your library</li>
+        <li>Media format filessupported (*.mp3 *.wav *.flac *.mp4 *.mkv *.avi *.mov *.wmv *.flv *.webm)</li>
+        <li>This will open your file explorer</li>
+        <li>Click where you keep your media files and open the folder where they are stored on your computer</li>
+        <li>You can add multiple files at once by holding down the up arrow key on your keyboard while selecting them and press open on your file explorer page. This will add all selected files to your media library database.</li>
+        <li>Note: The file explorer may take a moment to load the media files if you load too many files at once, it may cause the application to show as not responding just let it keep loading until the files are loaded and the system responds this is only a windows quirky behavior. We recommend adding arround 0-15 full album cd's or upto 100 single tracks at a time for better performance .</li>
+     </ul>
+
+
 
 
         <ul>
@@ -51,6 +59,7 @@ HELP_COL2 = """
         <li>Default password: <b>letmein</b></li>
         <li>Change your password after first login</li>
         <li>Never default your password with others</li>
+
     </ul>
         <ul>
         <li>Default password: <b>letmein</b></li>
